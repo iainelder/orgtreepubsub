548f105bd9bf8d310571d7cd3ffd84c04ce3896b
Iain Samuel McLean Elder <iain@isme.es>
2022-05-27 14:07:23 +0200

    Dump to GraphML format

--- dump_org.py
+++ dump_org.py
@@ -1,20 +1,13 @@
-from typing import Any, TextIO
 from boto3 import Session
 import networkx as nx  # type: ignore[import]
-import json
 import sys
 from org_graph import crawl_org_graph
 
 
 def main() -> None:
     graph = crawl_org_graph(Session())
-    cyto = nx.cytoscape_data(graph)
-    dump_json(cyto)
-
-
-def dump_json(data: Any, file: TextIO = sys.stdout) -> None:
-    json.dump(data, fp=file, indent=4)
-    file.write("\n")
+    # NetworkX writes GraphML in binary mode.
+    nx.write_graphml(graph, sys.stdout.buffer, encoding="utf8")
 
 
 if __name__ == "__main__":

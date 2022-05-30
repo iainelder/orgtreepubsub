c9e25ac1755e9a95192f31d55e3bb582ebda9252
Iain Samuel McLean Elder <iain@isme.es>
2022-05-30 09:16:59 +0200

    Draw from file and use twopi layout

--- draw_org.py
+++ draw_org.py
@@ -1,16 +1,20 @@
-from boto3 import Session
 import networkx as nx  # type: ignore[import]
 import matplotlib.pyplot as plt  # type: ignore[import]
-from org_graph import get_org_graph
-
+from org_browser import read_graphml
+import sys
+import matplotlib.pyplot as plt
+import networkx as nx
+from networkx.drawing.nx_pydot import graphviz_layout
 
 def main() -> None:
-    graph = get_org_graph(Session())
+    graph = read_graphml(sys.stdin.buffer)
     draw(graph)
 
 
 def draw(graph: nx.Graph) -> None:
-    nx.draw_networkx(graph)
+    # https://stackoverflow.com/questions/57512155/how-to-draw-a-tree-more-beautifully-in-networkx
+    pos = graphviz_layout(graph, prog="twopi")
+    nx.draw(graph, pos)
     plt.show()
 
 

2a998c4894ae273918b678ab33f9e091e5386567
Iain Samuel McLean Elder <iain@isme.es>
2022-05-30 09:16:13 +0200

    Move write function to graph module

--- dump_org.py
+++ dump_org.py
@@ -1,44 +1,11 @@
-from copy import deepcopy
-from typing import BinaryIO
 from boto3 import Session
-import networkx as nx  # type: ignore[import]
-import sys
-from org_graph import get_org_graph
+from org_graph import get_org_graph, write_graphml
 
 
 def main() -> None:
     "Write the organization graph to standard output as GraphML."
     org = get_org_graph(Session())
     write_graphml(org)
-
-
-def write_graphml(graph: nx.Graph, file: BinaryIO=sys.stdout.buffer) -> None:
-    """Write the organization graph to a file as GraphML.
-
-    By default write to standard output.
-
-    Nonscalar attributes, such as lists and datetimes, are omitted, because
-    NetworkX does not support them as data values.
-    """
-    graph_copy = deepcopy(graph)
-    delete_nonscalar_attributes(graph_copy)
-    # NetworkX writes GraphML in binary mode.
-    nx.write_graphml(graph_copy, file, encoding="utf8")
-
-
-def delete_nonscalar_attributes(graph: nx.Graph) -> None:
-    """Delete nonscalar atrributes from the graph.
-
-    NetworkX does not support nonscalar attibutes as data values.
-    """
-
-    for attrs in graph.nodes.values():
-        if attrs["type"] == "organization":
-            del attrs["AvailablePolicyTypes"]
-        if attrs["type"] == "root":
-            del attrs["PolicyTypes"]
-        if attrs["type"] == "account":
-            del attrs["JoinedTimestamp"]
 
 
 if __name__ == "__main__":
--- org_graph.py
+++ org_graph.py
@@ -1,3 +1,6 @@
+from copy import deepcopy
+import sys
+from typing import BinaryIO
 from pubsub import pub  # type: ignore[import]
 import networkx as nx  # type: ignore[import]
 from boto3 import Session
@@ -41,3 +44,36 @@ def add_account(graph: nx.DiGraph, resource: Account, parent: Parent) -> None:
     resource["type"] = "account"
     graph.add_node(resource["Id"], **resource)
     graph.add_edge(parent["Id"], resource["Id"])
+
+
+def read_graphml(file: BinaryIO) -> nx.DiGraph:
+    return nx.read_graphml(file)
+
+
+def write_graphml(graph: nx.Graph, file: BinaryIO=sys.stdout.buffer) -> None:
+    """Write the organization graph to a file as GraphML.
+
+    By default write to standard output.
+
+    Nonscalar attributes, such as lists and datetimes, are omitted, because
+    NetworkX does not support them as data values.
+    """
+    graph_copy = deepcopy(graph)
+    _delete_nonscalar_attributes(graph_copy)
+    # NetworkX writes GraphML in binary mode.
+    nx.write_graphml(graph_copy, file, encoding="utf8")
+
+
+def _delete_nonscalar_attributes(graph: nx.Graph) -> None:
+    """Delete nonscalar atrributes from the graph.
+
+    NetworkX does not support nonscalar attibutes as data values.
+    """
+
+    for attrs in graph.nodes.values():
+        if attrs["type"] == "organization":
+            del attrs["AvailablePolicyTypes"]
+        if attrs["type"] == "root":
+            del attrs["PolicyTypes"]
+        if attrs["type"] == "account":
+            del attrs["JoinedTimestamp"]

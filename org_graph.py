da52de57044e4b3742d98deece5b76eefcf44556
Iain Samuel McLean Elder <iain@isme.es>
2024-01-14 05:08:39 +0100

    Add orgunit tags to graph

--- org_graph.py
+++ org_graph.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+from dataclasses import dataclass, field
 from copy import deepcopy
 import sys
 from typing import Any, BinaryIO, Dict, Iterator, Union, cast
@@ -6,11 +8,73 @@ import networkx as nx  # type: ignore[import]
 from boto3 import Session
 from orgtreepubsub import crawl_organization
 
-from type_defs import Account, Org, OrgUnit, Root, Parent, Resource, Tag
+from type_defs import Org, Parent, Resource, Tag
+from mypy_boto3_organizations.type_defs import RootTypeDef, AccountTypeDef, OrganizationalUnitTypeDef
+
+# Creating my own data classes instead of exposing the ones generated for mypy is a step in the right direction.
+# But using them insidethe graph itself is awkward and complicated.
+# TODO: Just use attributes on the nodes internally and construct data classes for a result of queries.
+# That should also make it easier to query by tag.
+# I need to bring back the "tag:" prefix to make a tag namespace.
+
+@dataclass(frozen=True)
+class Root:
+    id: str
+    arn: str
+    name: str
+    tags: Dict[str, str] = field(default_factory=dict)
+
+    @staticmethod
+    def from_boto3(root: RootTypeDef) -> "Root":
+        return Root(id=root["Id"], arn=root["Arn"], name=root["Name"])
+
+    def add_tag(self, tag: Tag) -> None:
+        self.tags[tag["Key"]] = tag["Value"]
+
+
+@dataclass(frozen=True)
+class Account:
+    id: str
+    arn: str
+    name: str
+    email: str
+    joined_timestamp: datetime
+    tags: Dict[str, str] = field(default_factory=dict)
+
+    @staticmethod
+    def from_boto3(account: AccountTypeDef) -> "Account":
+        return Account(
+            id=account["Id"],
+            arn=account["Arn"],
+            name=account["Name"],
+            email=account["Email"],
+            joined_timestamp=account["JoinedTimestamp"]
+        )
+
+    def add_tag(self, tag: Tag) -> None:
+        self.tags[tag["Key"]] = tag["Value"]
+
+
+@dataclass(frozen=True)
+class OrgUnit:
+    id: str
+    arn: str
+    name: str
+    tags: Dict[str, str] = field(default_factory=dict)
+
+    @staticmethod
+    def from_boto3(account: OrganizationalUnitTypeDef) -> "OrgUnit":
+        return OrgUnit(
+            id=account["Id"],
+            arn=account["Arn"],
+            name=account["Name"],
+        )
+
+    def add_tag(self, tag: Tag) -> None:
+        self.tags[tag["Key"]] = tag["Value"]
+
 
 class OrgGraph:
-
-    management_account_id: str
 
     def __init__(self, graph: nx.DiGraph) -> None:
         """Don't use this initializer. Instead use `snapshot_org`."""
@@ -18,42 +82,58 @@ class OrgGraph:
 
     @property
     def management_account(self) -> Account:
-        # The graph data is a copy of the description dict with an extra type key
-        # for the resource type.
-        data: Dict[str, Any] = self._graph.nodes[self._management_account_id]
-        account = cast(Account, {k: v for k, v in data.items() if k != "type"})
-        return account
+        for account in nx.get_node_attributes(self._graph, "account").values():
+            if account.id == self._management_account_id:
+                return cast(Account, account)
+        raise AssertionError("missed management account")
 
     @property
     def root(self) -> Root:
-        data: Dict[str, Any] = self._graph.nodes[self._root_id]
-        root = cast(Root, {k: v for k, v in data.items() if k != "type"})
-        return root
+        for root in nx.get_node_attributes(self._graph, "root").values():
+            return cast(Root, root)
+        raise AssertionError("missed root")
 
     def _set_org_metadata(self, org: Org) -> None:
         self._management_account_id = org["MasterAccountId"]
         self.organization_id = org["Id"]
 
-    def _set_root_metadata(self, org: Org, resource: Root) -> None:
-        self._root_id = resource["Id"]
+    def account(self, id: str) -> Account:
+        for account in nx.get_node_attributes(self._graph, "account").values():
+            if account.id == id:
+                return cast(Account, account)
+        raise ValueError(id)
 
-    def ac(self, account_id: str) -> Account:
-        data: Dict[str, Any] = self._graph.nodes[account_id]
-        if data["type"] != "account":
-            raise AssertionError(f"{account_id=} is {data['type']=}, not account")
-        account = cast(Account, {k: v for k, v in data.items() if k != "type"})
-        return account
+    def orgunit(self, name: str) -> OrgUnit:
+        for orgunit in nx.get_node_attributes(self._graph, "organizational_unit").values():
+            if orgunit.name == name:
+                return cast(OrgUnit, orgunit)
+        raise ValueError(id)
 
+    def add_root(self, resource: RootTypeDef, org: Org) -> None:
+        self._graph.add_node(resource["Id"], root=Root.from_boto3(resource))
+        self._graph.add_edge(org["Id"], resource["Id"])
+
+    def add_organizational_unit(self, resource: OrganizationalUnitTypeDef, parent: Parent) -> None:
+        self._graph.add_node(resource["Id"], organizational_unit=OrgUnit.from_boto3(resource))
+        self._graph.add_edge(parent["Id"], resource["Id"])
+
+    def add_account(self, resource: AccountTypeDef, parent: Parent) -> None:
+        self._graph.add_node(resource["Id"], account=Account.from_boto3(resource))
+        self._graph.add_edge(parent["Id"], resource["Id"])
+
+    def add_tag(self, resource: Resource, tag: Tag) -> None:
+        for resource_ in self._graph.nodes[resource["Id"]].values():
+            resource_.add_tag(tag)
 
 def snapshot_org(session: Session) -> OrgGraph:
     internal_graph = nx.DiGraph()
     org_model = OrgGraph(internal_graph)
 
     pub.subscribe(org_model._set_org_metadata, "organization")
-    pub.subscribe(add_root, "root", graph=internal_graph)
-    pub.subscribe(org_model._set_root_metadata, "root")
-    pub.subscribe(add_organizational_unit, "organizational_unit", graph=internal_graph)
-    pub.subscribe(add_account, "account", graph=internal_graph)
+    pub.subscribe(org_model.add_root, "root")
+    pub.subscribe(org_model.add_organizational_unit, "organizational_unit")
+    pub.subscribe(org_model.add_account, "account")
+    pub.subscribe(org_model.add_tag, "tag")
 
     crawl_organization(session)
 
@@ -92,17 +172,17 @@ def add_organization(graph: nx.DiGraph, org: Org) -> None:
     graph.add_node(org["Id"], **org, type="organization")
 
 
-def add_root(graph: nx.DiGraph, resource: Root, org: Org) -> None:
+def add_root(graph: nx.DiGraph, resource: RootTypeDef, org: Org) -> None:
     graph.add_node(resource["Id"], **resource, type="root")
     graph.add_edge(org["Id"], resource["Id"])
 
 
-def add_organizational_unit(graph: nx.DiGraph, resource: OrgUnit, parent: Parent) -> None:
+def add_organizational_unit(graph: nx.DiGraph, resource: OrganizationalUnitTypeDef, parent: Parent) -> None:
     graph.add_node(resource["Id"], **resource, type="organizational_unit")
     graph.add_edge(parent["Id"], resource["Id"])
 
 
-def add_account(graph: nx.DiGraph, resource: Account, parent: Parent) -> None:
+def add_account(graph: nx.DiGraph, resource: AccountTypeDef, parent: Parent) -> None:
     graph.add_node(resource["Id"], **resource, type="account")
     graph.add_edge(parent["Id"], resource["Id"])
 
--- test_graph_organization.py
+++ test_graph_organization.py
@@ -1,7 +1,7 @@
 import pytest
 from boto3 import Session
 import boto3
-from org_graph import snapshot_org
+from org_graph import snapshot_org, Account, OrgUnit
 
 
 @pytest.fixture(autouse=True)
@@ -15,7 +15,7 @@ def test_new_org_has_mgmt_account_property() -> None:
     mgmt_desc = client.describe_account(AccountId=org_desc["MasterAccountId"])["Account"]
 
     graph = snapshot_org(Session())
-    assert graph.management_account == mgmt_desc
+    assert graph.management_account == Account.from_boto3(mgmt_desc)
 
 
 def test_new_org_has_organization_id_property() -> None:
@@ -31,13 +31,42 @@ def test_new_org_has_root_property() -> None:
     root_desc = client.list_roots()["Roots"][0]
 
     graph = snapshot_org(Session())
-    assert graph.root == root_desc
+    assert graph.root.id == root_desc["Id"]
+    assert graph.root.arn == root_desc["Arn"]
+    assert graph.root.name == root_desc["Name"]
 
 
-def test_new_org_has_mgmt_account_via_ac() -> None:
+def test_new_org_has_mgmt_account_via_account() -> None:
     client = boto3.client("organizations")
     org_desc = client.describe_organization()["Organization"]
     mgmt_desc = client.describe_account(AccountId=org_desc["MasterAccountId"])["Account"]
 
     graph = snapshot_org(Session())
-    assert graph.ac(org_desc["MasterAccountId"]) == mgmt_desc
+    assert graph.account(id=org_desc["MasterAccountId"]) == Account.from_boto3(mgmt_desc)
+
+
+def test_has_tagged_root() -> None:
+    client = boto3.client("organizations")
+    root = client.list_roots()["Roots"][0]
+    client.tag_resource(ResourceId=root["Id"], Tags=[{"Key": "Key1", "Value": "Value1"}])
+
+    graph = snapshot_org(Session())
+    assert graph.root.tags["Key1"] == "Value1"
+
+
+def test_has_orgunit() -> None:
+    client = boto3.client("organizations")
+    root = client.list_roots()["Roots"][0]
+    orgunit = client.create_organizational_unit(ParentId=root["Id"], Name="OU1")["OrganizationalUnit"]
+
+    graph = snapshot_org(Session())
+    assert graph.orgunit(name="OU1") == OrgUnit.from_boto3(orgunit)
+
+
+def test_has_tagged_orgunit() -> None:
+    client = boto3.client("organizations")
+    root = client.list_roots()["Roots"][0]
+    client.create_organizational_unit(ParentId=root["Id"], Name="OU1", Tags=[{"Key": "Key1", "Value": "Value1"}])
+
+    graph = snapshot_org(Session())
+    assert graph.orgunit(name="OU1").tags["Key1"] == "Value1"

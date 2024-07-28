9e64abdb9b74ffd0226fd1e67a41ea9f64d344a7
Iain Samuel McLean Elder <iain@isme.es>
2024-07-28 16:35:20 +0200

    Add public types to top-level module

---
+++ orgtreepubsub/__init__.py
@@ -0,0 +1,23 @@
+from .orgtreepubsub import OrgCrawler
+from .type_defs import (
+    Account,
+    Org,
+    Organization,
+    OrgUnit,
+    Parent,
+    Resource,
+    Root,
+    Tag,
+)
+
+__all__ = [
+    "Account",
+    "Org",
+    "Organization",
+    "OrgCrawler",
+    "OrgUnit",
+    "Parent",
+    "Resource",
+    "Root",
+    "Tag",
+]
